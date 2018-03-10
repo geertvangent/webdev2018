a127d9e061b764797c79dbe940b559b486013a69
geertvangent <geertvangent@gmail.com>
2018-03-10 16:13:57 +0100

    Initial commit

---
+++ pages/pagebuilder.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: geertvangent
+ * Date: 10/03/2018
+ * Time: 10:03
+ */
+
+ abstract class PageBuilder{
+
+     public $id = 0;
+
+     static function showTitle(){
+         echo "Title";
+     }
+
+     static function showNav(){
+         echo "Navigation";
+     }
+
+     static function showMain(){
+         echo "Main";
+     }
+
+     static function showAside(){
+         echo "Aside";
+     }
+
+     static function showFooter(){
+            echo "&copy; Geert Van Gent";
+        }
+
+
+
+
+
+ };
+
+
+
+?>
