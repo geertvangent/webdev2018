75e5182285a33888305c3b2af9881c84a01efbab
Yota2 <yoni.jmr@gmail.com>
2018-03-17 12:10:44 +0100

    what I CHANGED IN THIS VERSION

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<? include 'pages/pagebuilder.php' ?>
+<?php include 'pages/pagebuilder.php' ?>
 <!doctype html>
 <html class="no-js" lang="">
     <head>
@@ -23,26 +23,29 @@
         <!-- Add your site or application content here -->
 
         <header>
-            <h1><? PageBuilder::showTitle(); ?></h1>
+            <h1><?php PageBuilder::showTitle(); ?></h1>
             <nav>
-                <?
+                <?php
                     PageBuilder::showNav();
                 ?>
             </nav>
         </header>
 
         <main>
-            <?
+            <?php
                 PageBuilder::showMain();
+                echo "hallo Syntra";
             ?>
 
 
         </main>
 
         <aside>
-            <?
+            <?php
                 PageBuilder::showAside();
+                echo  "Hallo PhP";
             ?>
+
 
         </aside>
 
--- pages/pagebuilder.php
+++ pages/pagebuilder.php
@@ -22,6 +22,7 @@
          echo "Main";
      }
 
+
      static function showAside(){
          echo "Aside";
      }
