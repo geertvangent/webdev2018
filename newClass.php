d2efbfbd1e97607dc734e7089478c8df08ca922a
Sophie <sophielenoir5@hotmail.com>
2018-03-17 11:58:41 +0100

    Les 2

---
+++ newClass.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 17-Mar-18
+ * Time: 9:10 AM
+ */
+
+/* multiline comment */
+// oneline comment
+
+class NewClass{
+    const SUPERHERO ="Sphie";
+    private $necessaryData;
+    function __construct($data){
+        $this->necessaryData = $data;
+    }
+    function canFly(){
+        echo "Tjilp";
+    }
+}
+
+?>
