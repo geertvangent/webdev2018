1db28f3a1519b09e11f8767b6c2e93d40c29ed9e
Sophie <sophielenoir5@hotmail.com>
2018-03-17 12:02:31 +0100

    Les 2

--- index.php
+++ index.php
@@ -1,65 +1,69 @@
-<? include 'pages/pagebuilder.php' ?>
+<?php include 'pages/pagebuilder.php' ?>
+<?php include 'newClass.php'?>
 <!doctype html>
 <html class="no-js" lang="">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title></title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
 
-        <link rel="apple-touch-icon" href="icon.png">
-        <!-- Place favicon.ico in the root directory -->
+    <link rel="apple-touch-icon" href="icon.png">
+    <!-- Place favicon.ico in the root directory -->
 
-        <link rel="stylesheet" href="css/normalize.css">
-        <link rel="stylesheet" href="css/main.css">
-    </head>
-    <body>
-        <!--[if lte IE 9]>
-            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
-        <![endif]-->
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+<!--[if lte IE 9]>
+<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+<![endif]-->
 
-        <!-- Add your site or application content here -->
+<!-- Add your site or application content here -->
 
-        <header>
-            <h1><? PageBuilder::showTitle(); ?></h1>
-            <nav>
-                <?
-                    PageBuilder::showNav();
-                ?>
-            </nav>
-        </header>
-
-        <main>
-            <?
-                PageBuilder::showMain();
-            ?>
-
-
-        </main>
-
-        <aside>
-            <?
-                PageBuilder::showAside();
-            ?>
-
-        </aside>
-
-        <footer>
+<header>
+    <h1><?php PageBuilder::showTitle(); ?></h1>
+    <nav>
         <?php
-                PageBuilder::showFooter();
-            /*$footer = new Footer();
-            $footer->showFooter();*/
+        PageBuilder::showNav();
         ?>
-        </footer>
+    </nav>
+</header>
 
-        <!-- <script src="js/vendor/modernizr-3.5.0.min.js"></script> -->
-        <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.2.1.min.js"><\/script>')</script>
-        <script src="js/plugins.js"></script>
-        <script src="js/main.js"></script>
+<main>
+    <?php
+    PageBuilder::showMain();
+    ?>
+    <?php
+    $superHeroMan= new NewClass( "nestring");
+    ?>
 
 
-    </body>
+</main>
+
+<aside>
+    <?php
+    PageBuilder::showAside();
+    ?>
+
+</aside>
+
+<footer>
+    <?php
+    PageBuilder::showFooter();
+    //$footer = new Footer();
+    //$footer->showFooter();
+    ?>
+</footer>
+
+<!-- <script src="js/vendor/modernizr-3.5.0.min.js"></script> -->
+<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+<script>window.jQuery || document.write('<script src="js/vendor/jquery-3.2.1.min.js"><\/script>')</script>
+<script src="js/plugins.js"></script>
+<script src="js/main.js"></script>
+
+
+</body>
 </html>
