079d156c8ee6a09631ae4662db92221a6b69d05c
Jonas Laurens <lauwe505@gmail.com>
2018-03-17 10:49:30 +0100

    first

---
+++ pages/newClass.php
@@ -0,0 +1,28 @@
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jonas
+ * Date: 17/03/2018
+ * Time: 9:47
+ */
+
+// one line comment
+
+
+class NewClass {
+    const superHero = "Barry Allen";
+    private $necessaryData;
+
+    function __construct( $data) {
+        $this->necessaryData = $data;
+    }
+
+    function canFly() {
+        echo "the Flash";
+    }
+
+}
+
+// test test
+?>
